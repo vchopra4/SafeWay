bcd17dd3adb51f1ada554cf35d970371a867c29c
Ningsong Shen <12574693+ningsongshen@users.noreply.github.com>
2019-11-23 16:10:31 -0500

    form submits data to react

--- Frontend/safeway/src/index.js
+++ Frontend/safeway/src/index.js
@@ -16,15 +16,36 @@ class Topbar extends React.Component {
 }
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {start: '', end: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmit(event) {
+        // this line represents the information for the post request
+        alert('Start: ' + this.state.start + ", End: " + this.state.end);
+        event.preventDefault();
+    }
     render() {
         return (
-            <div className="search">
-                <input className="start" placeholder="Start"></input>
+            <form className="search" onSubmit={this.handleSubmit}>
+                <input className="start" name="start" placeholder="Start" value={this.state.start} onChange={this.handleChange}></input>
                 
-                <input className="end" placeholder="End"></input>
+                <input className="end" name="end" placeholder="End" value={this.state.end} onChange={this.handleChange}></input>
 
-                <input className="submit" type="submit"></input>
-            </div>
+                <input className="submit" type="submit" value="Submit"></input>
+            </form>
             
         )
     }
