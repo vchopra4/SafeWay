c47f23c0b6827d9c3f5ffa778f01f5f03969733c
Keven-Zhu <zhukeven86@gmail.com>
2019-11-23 13:49:22 -0500

    Added google maps and path drawing function to the front end

---
+++ Frontend/safeway/src/Map.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Polyline
+} from "react-google-maps";
+
+class Map extends React.Component {
+  path = [
+    { lat: 18.558908, lng: -68.389916 },
+    { lat: 18.558853, lng: -68.389922 },
+    { lat: 18.558375, lng: -68.389729 },
+    { lat: 18.558032, lng: -68.389182 },
+    { lat: 18.55805, lng: -68.388613 },
+    { lat: 18.558256, lng: -68.388213 },
+    { lat: 18.558744, lng: -68.387929 }
+  ];
+
+path2=[{ lat1: 18.558908, lng1: -68.389916, lat2: 18.558853, lng2: -68.389922}]
+
+  drawPath = () => {
+    let pathLine = [];
+
+    let i = 0;
+
+    return (
+      <React.Fragment>
+        {this.path2.map(step => (
+          <Polyline
+            key={i++}
+            path={[{lat: step.lat1, lng: step.lng1}, {lat: step.lat2, lng: step.lng2}]}
+            options={{ strokeColor: "#FF0000" }}
+          />
+        ))}
+      </React.Fragment>
+    );
+
+    return pathLine;
+  };
+
+  render = () => {
+    return (
+      <GoogleMap
+        defaultZoom={16}
+        defaultCenter={{ lat: 18.559008, lng: -68.388881 }}
+      >
+        {this.drawPath()}
+        {/* <Polyline path={this.path} options={{ strokeColor: "#FF0000 " }} /> */}
+      </GoogleMap>
+    );
+  };
+}
+
+const MapComponent = withScriptjs(withGoogleMap(Map));
+
+export default () => (
+  <MapComponent
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA05IZP-_GDgylMrM22XBYZ9qUxiTXjq-w&v=3.exp&libraries=geometry,drawing,places"
+    loadingElement={<div style={{ height: `100%` }} />}
+    containerElement={<div style={{ height: `400px`, width: "500px" }} />}
+    mapElement={<div style={{ height: `100%` }} />}
+  />
+);
--- Frontend/safeway/src/index.js
+++ Frontend/safeway/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
 import './index.css';
+import Map from './Map'
 
 class Topbar extends React.Component {
     render() {
@@ -24,17 +25,6 @@ class Search extends React.Component {
             </div>
             
         )
-    }
-}
-class Map extends React.Component {
-    render() {
-        return (
-            <div class="map">
-                <iframe width="100%" height="450" frameborder="0" style={{border:0}}
-src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0SvEag7uLogRm3m5qlPzh6o&key=..." allowfullscreen></iframe>
-            </div>
-        )
-        
     }
 }
 class App extends React.Component {
